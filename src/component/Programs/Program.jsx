b62be9054d7d440c55ddf4811f8baaff20058d7f
Nikunj Vadher <nikunjjv27@gmail.com>
2022-11-30 18:36:54 +0530

    adding Programs components

---
+++ src/component/Programs/Program.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Program.css';
+import { programsData } from '../../data/programsData'
+import rightArrow from '../../assets/rightArrow.png'
+
+const Program = () => {
+    return (
+        <>
+            <div className="programs" id="programs">
+
+                <div className="programs-header">
+                    <span className='stroke-text'>Explore our</span>
+                    <span>Programs </span>
+                    <span className='stroke-text'>to Shape You</span>
+                </div>
+
+                <div className="programs-categories">
+                    {programsData.map((program) => {
+                        return (
+                        <div className="category">
+                            {program.image}
+                            <span>{program.heading}</span>
+                            <span>{program.details}</span>
+                            <div className="join-now">
+                                <span>Join Now</span>
+                                <img src={rightArrow} alt="" />
+                            </div>
+                        </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Program
